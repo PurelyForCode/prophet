f3b1d27863c176f5274c916d58566fd49ad3dea9
PurelyForCode <purelyforcode@gmail.com>
2025-12-03 19:30:58 +0800

    remove supplier too

--- testing/ResetPrototype.ts
+++ testing/ResetPrototype.ts
@@ -2,6 +2,7 @@ import { Knex } from "knex"
 
 export async function resetPrototype(knex: Knex) {
 	await knex("product_group").delete()
+	await knex("supplier").delete()
 	await knex("sale").delete()
 	await knex("account").delete()
 }
