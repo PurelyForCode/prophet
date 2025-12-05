2befc47e55150f895e83e98dd417bb46b9fb8089
davidjamesdelacruz <davidjamesdelacruz.basc@gmal.com>
2025-12-05 09:52:31 +0800

    back to default

--- src/application/excel/import_products/Usecase.ts
+++ src/application/excel/import_products/Usecase.ts
@@ -120,7 +120,7 @@ export class ImportProductsUsecase
 									await createGroupUsecase.call({
 										accountId: input.accountId,
 										categoryId: null,
-										groupName: groupName,
+										name: groupName,
 										setting: null,
 									})
 								},
